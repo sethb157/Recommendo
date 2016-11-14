11cf53ab859fdb214ffdd52c0c2a97a50932c8d6
cpirvu <costin_64@yahoo.com>
2016-11-14 09:30:38 -0800

    Initial commit

---
+++ app/src/main/java/edu/calpoly/cpirvu/preferencessetup/Preferences.java
@@ -0,0 +1,81 @@
+package edu.calpoly.cpirvu.preferencessetup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class Preferences extends AppCompatActivity {
+
+    public static ArrayList<String> prefList;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+
+        prefList = (ArrayList<String>) getLastCustomNonConfigurationInstance();
+
+        if (prefList == null) {
+            prefList = new ArrayList<String>();
+        }
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(Preferences.this, "The position is " + position, Toast.LENGTH_SHORT).show();
+                if (position == 0) {
+                    if (prefList.contains("biking")) {
+                        prefList.remove("biking");
+                    }
+                    else {
+                        prefList.add("biking");
+                    }
+                }
+                else if (position == 1) {
+                    if (prefList.contains("coffee")) {
+                        prefList.remove("coffee");
+                    }
+                    else {
+                        prefList.add("coffee");
+                    }
+                }
+                else if (position == 2) {
+                    if (prefList.contains("fitness")) {
+                        prefList.remove("fitness");
+                    }
+                    else {
+                        prefList.add("fitness");
+                    }
+                }
+                else if (position == 3) {
+                    if (prefList.contains("running")) {
+                        prefList.remove("running");
+                    }
+                    else {
+                        prefList.add("running");
+                    }
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public ArrayList<String> onRetainCustomNonConfigurationInstance() {
+        return prefList;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LOGGING!!!", prefList.get(0));
+    }
+}
