4d3724b12e6995a931457e88a6276914aa838ddf
cpirvu <costin_64@yahoo.com>
2016-11-22 10:21:05 -0800

    new icons and button to move to next activity

--- app/src/main/java/edu/calpoly/recommendo/activities/Preferences.java
+++ app/src/main/java/edu/calpoly/recommendo/activities/Preferences.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -20,7 +21,12 @@ public class Preferences extends AppCompatActivity {
     public static final String COFFEE = "coffee";
     public static final String FITNESS = "fitness";
     public static final String RUNNING = "running";
-
+    public static final String RESTAURANT = "restaurant";
+    public static final String SWIMMING = "swimming";
+    public static final String MOVIES = "movies";
+    public static final String PIZZA = "pizza";
+    public static final String HIKING = "hiking";
+    public static final String GOLF = "golf";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,9 +42,11 @@ public class Preferences extends AppCompatActivity {
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
 
+        Button doneButton = (Button) findViewById(R.id.preferences_done);
+
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(Preferences.this, "The position is " + position, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(Preferences.this, "The position is " + position, Toast.LENGTH_SHORT).show();
                 if (position == 0) {
                     if (prefList.contains(BIKING)) {
                         prefList.remove(BIKING);
@@ -71,9 +79,63 @@ public class Preferences extends AppCompatActivity {
                         prefList.add(RUNNING);
                     }
                 }
+                else if (position == 4) {
+                    if (prefList.contains(RESTAURANT)) {
+                        prefList.remove(RESTAURANT);
+                    }
+                    else {
+                        prefList.add(RESTAURANT);
+                    }
+                }
+                else if (position == 5) {
+                    if (prefList.contains(SWIMMING)) {
+                        prefList.remove(SWIMMING);
+                    }
+                    else {
+                        prefList.add(SWIMMING);
+                    }
+                }
+                else if (position == 6) {
+                    if (prefList.contains(MOVIES)) {
+                        prefList.remove(MOVIES);
+                    }
+                    else {
+                        prefList.add(MOVIES);
+                    }
+                }
+                else if (position == 7) {
+                    if (prefList.contains(PIZZA)) {
+                        prefList.remove(PIZZA);
+                    }
+                    else {
+                        prefList.add(PIZZA);
+                    }
+                }
+                else if (position == 8) {
+                    if (prefList.contains(HIKING)) {
+                        prefList.remove(HIKING);
+                    }
+                    else {
+                        prefList.add(HIKING);
+                    }
+                }
+                else if (position == 9) {
+                    if (prefList.contains(GOLF)) {
+                        prefList.remove(GOLF);
+                    }
+                    else {
+                        prefList.add(GOLF);
+                    }
+                }
             }
         });
 
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //launch new intent
+            }
+        });
 
 
     }
