7aaeeefe55ebe33537612ba2781b3c83378610ef
cpirvu <costin_64@yahoo.com>
2016-11-27 22:54:32 -0800

    better detailed suggestions page

--- app/src/main/java/edu/calpoly/recommendo/activities/DetailSuggestionActivity.java
+++ app/src/main/java/edu/calpoly/recommendo/activities/DetailSuggestionActivity.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import edu.calpoly.recommendo.R;
+import edu.calpoly.recommendo.suggestions.Suggestion;
+import edu.calpoly.recommendo.suggestions.SuggestionsManager;
 
 public class DetailSuggestionActivity extends Activity {
+
+    private SuggestionsManager suggestionsManager;
+
     private TextView name;
     private TextView address;
     private TextView type;
@@ -21,11 +26,18 @@ public class DetailSuggestionActivity extends Activity {
 
         index = getIntent().getIntExtra("key", 0);
 
-        //grab current suggestion
+        suggestionsManager = SuggestionsManager.getSharedManager();
+
+        final Suggestion s = suggestionsManager.getSuggestions().get(index);
 
         name = (TextView) findViewById(R.id.name);
         address = (TextView) findViewById(R.id.address);
         type = (TextView) findViewById(R.id.type);
         category = (TextView) findViewById(R.id.category);
+
+        name.setText(s.getName());
+        address.setText(s.getAddress());
+        type.setText(s.getType());
+        category.setText(s.getCategory());
     }
 }
