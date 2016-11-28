69f99646d94d7078ea2da29af3eac5189f7b7923
DanielCKennedy <danielckennedy@hotmail.com>
2016-11-27 17:59:56 -0800

    Adds Maps Page

---
+++ app/src/main/java/edu/calpoly/recommendo/activities/MapActivity.java
@@ -0,0 +1,42 @@
+package edu.calpoly.recommendo.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Marker;
+
+import edu.calpoly.recommendo.R;
+
+/**
+ * Created by Dan on 11/27/2016.
+ */
+
+public class MapActivity extends FragmentActivity
+    implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+}
