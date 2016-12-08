a112662ffba95c369cd2b6fc0aee911821fd482d
cpirvu <costin_64@yahoo.com>
2016-12-07 22:15:22 -0800

    better looks for detailed page

--- app/src/main/java/edu/calpoly/recommendo/activities/DetailSuggestionActivity.java
+++ app/src/main/java/edu/calpoly/recommendo/activities/DetailSuggestionActivity.java
@@ -22,6 +22,9 @@ public class DetailSuggestionActivity extends Activity {
     private TextView type;
     private TextView category;
     private ImageView imageView;
+    private ImageView address_icon;
+    private ImageView type_icon;
+    private ImageView category_icon;
     private int index;
 
     @Override
@@ -40,11 +43,18 @@ public class DetailSuggestionActivity extends Activity {
         type = (TextView) findViewById(R.id.type);
         category = (TextView) findViewById(R.id.category);
         imageView = (ImageView) findViewById(R.id.detail_image_view);
+        address_icon = (ImageView) findViewById(R.id.address_icon);
+        type_icon = (ImageView) findViewById(R.id.type_icon);
+        category_icon = (ImageView) findViewById(R.id.category_icon);
 
         name.setText(s.getName());
         address.setText(s.getAddress());
         type.setText(s.getType());
         category.setText(s.getCategory());
+
+        address_icon.setImageResource(R.drawable.address);
+        type_icon.setImageResource(R.drawable.type);
+        category_icon.setImageResource(R.drawable.category);
 
         // Load in picture if possible
         String picRef = s.getPicRef();
